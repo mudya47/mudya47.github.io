4e2d8be14b547c4ccedc855a349f67d2ed3b3457
Yoga1967 <adrianuseka67@gmail.com>
2023-01-05 14:26:08 +0700

    update

--- my-profile/src/About.js
+++ my-profile/src/About.js
@@ -4,7 +4,7 @@ import profile from './images/profile.jpg';
 function About(){
     return(
             <div class="contents bg-color-302f2f p-lr-20">
-                <div id="about "></div>
+                <div id="about"></div>
                 <h2 class="flex-color-white">About me</h2>
                 <div class="d-flex flex-flow-column-sp mw-1024 flex-color-white">
                     <div class="left flex-width-30 img-100 mb-20 pr-0">
